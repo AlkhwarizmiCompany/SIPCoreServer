43e823011f61d439cbae1847ff8ba3ea9089e49e
Magdy Mousa <MAjdi.M@alkhwarizmi.ai>
2023-11-27 14:48:06 +0200

    running server

--- SIPServer/MainWindow.xaml.cs
+++ SIPServer/MainWindow.xaml.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.VisualBasic.Logging;
-using SIPSorcery.Media;
+﻿using SIPSorcery.Media;
 using SIPSorcery.Net;
 using SIPSorcery.SIP;
 using SIPSorcery.SIP.App;
 using SIPSorceryMedia.Abstractions;
-using SIPSorceryMedia.Windows;
 using System.Net;
 using System.Runtime.Intrinsics.Arm;
 using System.Text;
@@ -27,166 +25,12 @@ namespace SIPServer
     {
         public MainWindow()
         {
+
             InitializeComponent();
 
-            
+            Server server = new Server();
+
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            string DEFAULT_DESTINATION_SIP_URI = "sip:Magdy@localhost:5060";
-
-            Console.WriteLine("SIPSorcery client user agent example.");
-            Console.WriteLine("Press ctrl-c to exit.");
-
-            // Plumbing code to facilitate a graceful exit.
-            ManualResetEvent exitMre = new ManualResetEvent(false);
-            bool preferIPv6 = false;
-            bool isCallHungup = false;
-            bool hasCallFailed = false;
-
-
-            SIPURI callUri = SIPURI.ParseSIPURI(DEFAULT_DESTINATION_SIP_URI);
-
-            Console.WriteLine($"Call destination {callUri}.");
-            
-
-            // Set up a default SIP transport.
-            var sipTransport = new SIPTransport();
-            sipTransport.PreferIPv6NameResolution = preferIPv6;
-            sipTransport.EnableTraceLogs();
-
-            var audioSession = new WindowsAudioEndPoint(new AudioEncoder());
-            audioSession.RestrictFormats(x => x.Codec == AudioCodecsEnum.PCMA || x.Codec == AudioCodecsEnum.PCMU);
-            //audioSession.RestrictFormats(x => x.Codec == AudioCodecsEnum.G722);
-            var rtpSession = new VoIPMediaSession(audioSession.ToMediaEndPoints());
-
-            var offerSDP = rtpSession.CreateOffer(preferIPv6 ? IPAddress.IPv6Any : IPAddress.Any);
-
-            // Create a client user agent to place a call to a remote SIP server along with event handlers for the different stages of the call.
-            var uac = new SIPClientUserAgent(sipTransport);
-            uac.CallTrying += (uac, resp) => Console.WriteLine($"{uac.CallDescriptor.To} Trying: {resp.StatusCode} {resp.ReasonPhrase}.");
-            uac.CallRinging += async (uac, resp) =>
-            {
-                Console.WriteLine($"{uac.CallDescriptor.To} Ringing: {resp.StatusCode} {resp.ReasonPhrase}.");
-                if (resp.Status == SIPResponseStatusCodesEnum.SessionProgress)
-                {
-                    if (resp.Body != null)
-                    {
-                        var result = rtpSession.SetRemoteDescription(SdpType.answer, SDP.ParseSDPDescription(resp.Body));
-                        if (result == SetDescriptionResultEnum.OK)
-                        {
-                            await rtpSession.Start();
-                            Console.WriteLine($"Remote SDP set from in progress response. RTP session started.");
-                        }
-                    }
-                }
-            };
-            uac.CallFailed += (uac, err, resp) =>
-            {
-                Console.WriteLine($"Call attempt to {uac.CallDescriptor.To} Failed: {err}");
-                hasCallFailed = true;
-            };
-            uac.CallAnswered += async (iuac, resp) =>
-            {
-                if (resp.Status == SIPResponseStatusCodesEnum.Ok)
-                {
-                    Console.WriteLine($"{uac.CallDescriptor.To} Answered: {resp.StatusCode} {resp.ReasonPhrase}.");
-
-                    if (resp.Body != null)
-                    {
-                        var result = rtpSession.SetRemoteDescription(SdpType.answer, SDP.ParseSDPDescription(resp.Body));
-                        if (result == SetDescriptionResultEnum.OK)
-                        {
-                            await rtpSession.Start();
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Failed to set remote description {result}.");
-                            uac.Hangup();
-                        }
-                    }
-                    else if (!rtpSession.IsStarted)
-                    {
-                        Console.WriteLine($"Failed to set get remote description in session progress or final response.");
-                        uac.Hangup();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"{uac.CallDescriptor.To} Answered: {resp.StatusCode} {resp.ReasonPhrase}.");
-                }
-            };
-
-            // The only incoming request that needs to be explicitly handled for this example is if the remote end hangs up the call.
-            sipTransport.SIPTransportRequestReceived += async (SIPEndPoint localSIPEndPoint, SIPEndPoint remoteEndPoint, SIPRequest sipRequest) =>
-            {
-                if (sipRequest.Method == SIPMethodsEnum.BYE)
-                {
-                    SIPResponse okResponse = SIPResponse.GetResponse(sipRequest, SIPResponseStatusCodesEnum.Ok, null);
-                    await sipTransport.SendResponseAsync(okResponse);
-
-                    if (uac.IsUACAnswered)
-                    {
-                        Console.WriteLine("Call was hungup by remote server.");
-                        isCallHungup = true;
-                        exitMre.Set();
-                    }
-                }
-            };
-
-            // Start the thread that places the call.
-            SIPCallDescriptor callDescriptor = new SIPCallDescriptor(
-                SIPConstants.SIP_DEFAULT_USERNAME,
-                null,
-                callUri.ToString(),
-                SIPConstants.SIP_DEFAULT_FROMURI,
-                callUri.CanonicalAddress,
-                null, null, null,
-            SIPCallDirection.Out,
-                SDP.SDP_MIME_CONTENTTYPE,
-                offerSDP.ToString(),
-                null);
-
-            uac.Call(callDescriptor, null);
-
-            // Ctrl-c will gracefully exit the call at any point.
-            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
-            {
-                e.Cancel = true;
-                exitMre.Set();
-            };
-
-            // Wait for a signal saying the call failed, was cancelled with ctrl-c or completed.
-            exitMre.WaitOne();
-
-            Console.WriteLine("Exiting...");
-
-            rtpSession.Close(null);
-
-            if (!isCallHungup && uac != null)
-            {
-                if (uac.IsUACAnswered)
-                {
-                    Console.WriteLine($"Hanging up call to {uac.CallDescriptor.To}.");
-                    uac.Hangup();
-                }
-                else if (!hasCallFailed)
-                {
-                    Console.WriteLine($"Cancelling call to {uac.CallDescriptor.To}.");
-                    uac.Cancel();
-                }
-
-                // Give the BYE or CANCEL request time to be transmitted.
-                Console.WriteLine("Waiting 1s for call to clean up...");
-                Task.Delay(1000).Wait();
-            }
-
-            if (sipTransport != null)
-            {
-                Console.WriteLine("Shutting down SIP transport...");
-                sipTransport.Shutdown();
-            }
-        }
     }
 }
---
+++ SIPServer/Server.cs
@@ -0,0 +1,234 @@
+﻿using System.Net;
+using Serilog;
+using Serilog.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using SIPSorcery;
+using SIPSorcery.Media;
+using SIPSorcery.Net;
+using SIPSorcery.SIP;
+using SIPSorcery.SIP.App;
+using SIPSorcery.Sys;
+using SIPSorceryMedia.Abstractions;
+
+namespace SIPServer
+{
+    struct SIPRegisterAccount
+    {
+        public string Username;
+        public string Password;
+        public string Domain;
+        public int Expiry;
+
+        public SIPRegisterAccount(string username, string password, string domain, int expiry)
+        {
+            Username = username;
+            Password = password;
+            Domain = domain;
+            Expiry = expiry;
+        }
+    }
+
+    internal class Server
+    {
+        private static SIPTransport _sipTransport;
+        private static int SIP_LISTEN_PORT = 5060;
+        private static Microsoft.Extensions.Logging.ILogger Log = NullLogger.Instance;
+        private static ConcurrentDictionary<string, SIPUserAgent> _calls = new ConcurrentDictionary<string, SIPUserAgent>();
+        private static ConcurrentDictionary<string, SIPRegisterAccount> registrations = new ConcurrentDictionary<string, SIPRegisterAccount>();
+
+        public Server()
+        {
+            _sipTransport = new SIPTransport();
+            _sipTransport.AddSIPChannel(new SIPUDPChannel(new IPEndPoint(IPAddress.Any, SIP_LISTEN_PORT)));
+
+
+            _sipTransport.EnableTraceLogs();
+
+            _sipTransport.SIPTransportRequestReceived += OnRequest;
+
+            Log = AddConsoleLogger();
+
+        }
+
+        private static async Task OnRequest(SIPEndPoint localSIPEndPoint, SIPEndPoint remoteEndPoint, SIPRequest sipRequest)
+        {
+            try
+            {
+                if (sipRequest.Header.From != null &&
+                sipRequest.Header.From.FromTag != null &&
+                sipRequest.Header.To != null &&
+                sipRequest.Header.To.ToTag != null)
+                {
+                    // This is an in-dialog request that will be handled directly by a user agent instance.
+                }
+                else if (sipRequest.Method == SIPMethodsEnum.INVITE)
+                {
+                    Log.LogInformation($"Incoming call request: {localSIPEndPoint}<-{remoteEndPoint} {sipRequest.URI}.");
+
+                    SIPUserAgent ua = new SIPUserAgent(_sipTransport, null);
+                    ua.OnCallHungup += OnHangup;
+                    //ua.ServerCallCancelled += (uas) => Log.LogDebug("Incoming call cancelled by remote party.");
+                    //ua.OnDtmfTone += (key, duration) => OnDtmfTone(ua, key, duration);
+                    //ua.OnRtpEvent += (evt, hdr) => Log.LogDebug($"rtp event {evt.EventID}, duration {evt.Duration}, end of event {evt.EndOfEvent}, timestamp {hdr.Timestamp}, marker {hdr.MarkerBit}.");
+                    ////ua.OnTransactionTraceMessage += (tx, msg) => Log.LogDebug($"uas tx {tx.TransactionId}: {msg}");
+                    //ua.ServerCallRingTimeout += (uas) =>
+                    //{
+                    //    Log.LogWarning($"Incoming call timed out in {uas.ClientTransaction.TransactionState} state waiting for client ACK, terminating.");
+                    //    ua.Hangup();
+                    //};
+
+                    var uas = ua.AcceptCall(sipRequest);
+                    var rtpSession = CreateRtpSession(ua, sipRequest.URI.User);
+
+                    // Insert a brief delay to allow testing of the "Ringing" progress response.
+                    // Without the delay the call gets answered before it can be sent.
+                    await Task.Delay(500);
+
+                    await ua.Answer(uas, rtpSession);
+
+                    if (ua.IsCallActive)
+                    {
+                        await rtpSession.Start();
+                        _calls.TryAdd(ua.Dialogue.CallId, ua);
+                    }
+                }
+                else if (sipRequest.Method == SIPMethodsEnum.BYE)
+                {
+                    SIPResponse byeResponse = SIPResponse.GetResponse(sipRequest, SIPResponseStatusCodesEnum.CallLegTransactionDoesNotExist, null);
+                    await _sipTransport.SendResponseAsync(byeResponse);
+                }
+                else if (sipRequest.Method == SIPMethodsEnum.SUBSCRIBE)
+                {
+                    SIPResponse notAllowededResponse = SIPResponse.GetResponse(sipRequest, SIPResponseStatusCodesEnum.MethodNotAllowed, null);
+                    await _sipTransport.SendResponseAsync(notAllowededResponse);
+                }
+                else if (sipRequest.Method == SIPMethodsEnum.OPTIONS || sipRequest.Method == SIPMethodsEnum.REGISTER)
+                {
+                    SIPRegisterAccount user;
+
+                    user.Username = sipRequest.Header.From.FromURI.User;
+                    user.Password = "xxx";
+                    user.Expiry = 1;
+                    user.Domain = "xyz";
+
+                    //string username = sipRequest.Header.From.FromURI.User;
+
+                    //// Extract the received password hash from the SIP request (for digest authentication)
+                    //string receivedPasswordHash = sipRequest.Header.AuthenticationHeaders[0].ToString();
+
+                    registrations.TryAdd($"{user.Username}@{user.Domain}", user);
+
+                    SIPResponse optionsResponse = SIPResponse.GetResponse(sipRequest, SIPResponseStatusCodesEnum.Ok, null);
+                    await _sipTransport.SendResponseAsync(optionsResponse);
+                }
+            }
+            catch (Exception reqExcp)
+            {
+                Log.LogWarning($"Exception handling {sipRequest.Method}. {reqExcp.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Example of how to create a basic RTP session object and hook up the event handlers.
+        /// </summary>
+        /// <param name="ua">The user agent the RTP session is being created for.</param>
+        /// <param name="dst">THe destination specified on an incoming call. Can be used to
+        /// set the audio source.</param>
+        /// <returns>A new RTP session object.</returns>
+        private static VoIPMediaSession CreateRtpSession(SIPUserAgent ua, string dst)
+        {
+            List<AudioCodecsEnum> codecs = new List<AudioCodecsEnum> { AudioCodecsEnum.PCMU, AudioCodecsEnum.PCMA, AudioCodecsEnum.G722 };
+
+            var audioSource = AudioSourcesEnum.SineWave;
+            if (string.IsNullOrEmpty(dst) || !Enum.TryParse(dst, out audioSource))
+            {
+                audioSource = AudioSourcesEnum.Music;
+            }
+
+            Log.LogInformation($"RTP audio session source set to {audioSource}.");
+
+            AudioExtrasSource audioExtrasSource = new AudioExtrasSource(new AudioEncoder(), new AudioSourceOptions { AudioSource = audioSource });
+            audioExtrasSource.RestrictFormats(formats => codecs.Contains(formats.Codec));
+            var rtpAudioSession = new VoIPMediaSession(new MediaEndPoints { AudioSource = audioExtrasSource });
+            rtpAudioSession.AcceptRtpFromAny = true;
+
+            // Wire up the event handler for RTP packets received from the remote party.
+            rtpAudioSession.OnRtpPacketReceived += (ep, type, rtp) => OnRtpPacketReceived(ua, type, rtp);
+            rtpAudioSession.OnTimeout += (mediaType) =>
+            {
+                if (ua?.Dialogue != null)
+                {
+                    Log.LogWarning($"RTP timeout on call with {ua.Dialogue.RemoteTarget}, hanging up.");
+                }
+                else
+                {
+                    Log.LogWarning($"RTP timeout on incomplete call, closing RTP session.");
+                }
+
+                ua.Hangup();
+            };
+
+            return rtpAudioSession;
+        }
+
+
+        /// <summary>
+        /// Event handler for receiving RTP packets.
+        /// </summary>
+        /// <param name="ua">The SIP user agent associated with the RTP session.</param>
+        /// <param name="type">The media type of the RTP packet (audio or video).</param>
+        /// <param name="rtpPacket">The RTP packet received from the remote party.</param>
+        private static void OnRtpPacketReceived(SIPUserAgent ua, SDPMediaTypesEnum type, RTPPacket rtpPacket)
+        {
+            // The raw audio data is available in rtpPacket.Payload.
+        }
+
+        /// <summary>
+        /// Event handler for receiving a DTMF tone.
+        /// </summary>
+        /// <param name="ua">The user agent that received the DTMF tone.</param>
+        /// <param name="key">The DTMF tone.</param>
+        /// <param name="duration">The duration in milliseconds of the tone.</param>
+        private static void OnDtmfTone(SIPUserAgent ua, byte key, int duration)
+        {
+            string callID = ua.Dialogue.CallId;
+            Log.LogInformation($"Call {callID} received DTMF tone {key}, duration {duration}ms.");
+        }
+
+
+        /// <summary>
+        /// Remove call from the active calls list.
+        /// </summary>
+        /// <param name="dialogue">The dialogue that was hungup.</param>
+        private static void OnHangup(SIPDialogue dialogue)
+        {
+            if (dialogue != null)
+            {
+                string callID = dialogue.CallId;
+                if (_calls.ContainsKey(callID))
+                {
+                    if (_calls.TryRemove(callID, out var ua))
+                    {
+                        // This app only uses each SIP user agent once so here the agent is 
+                        // explicitly closed to prevent is responding to any new SIP requests.
+                        ua.Close();
+                    }
+                }
+            }
+        }
+
+        private static Microsoft.Extensions.Logging.ILogger AddConsoleLogger()
+        {
+            var serilogLogger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Is(Serilog.Events.LogEventLevel.Debug)
+                .WriteTo.Console()
+                .CreateLogger();
+            var factory = new SerilogLoggerFactory(serilogLogger);
+            SIPSorcery.LogFactory.Set(factory);
+            return factory.CreateLogger<Server>();
+        }
+    }
+}
